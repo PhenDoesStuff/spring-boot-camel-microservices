ccfd1c56d123485ebfcd98b303b6048466b989b1
Stephen Montague <stephen.d.montgue@gmail.com>
2022-01-20 14:49:09 -0500

    Created first route

--- camel-microservice-a/src/main/java/com/montaguestephen/microservices/camelmicroservicea/CamelMicroserviceAApplication.java
+++ camel-microservice-a/src/main/java/com/montaguestephen/microservices/camelmicroservicea/CamelMicroserviceAApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CamelMicroserviceAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CamelMicroserviceAApplication.class, args);
+        SpringApplication.run(CamelMicroserviceAApplication.class, args);
 	}
 
 }
---
+++ camel-microservice-a/src/main/java/com/montaguestephen/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -0,0 +1,18 @@
+package com.montaguestephen.microservices.camelmicroservicea.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstTimerRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // use a timer to trigger messages
+        // transformation
+        // log it to the console
+
+        // Created a timer endpoint below
+        from("timer:first-timer").to("log:first-timer");
+    }
+}
